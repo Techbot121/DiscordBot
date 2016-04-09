0414ab4d09af4b69dc2354e19d345337de5c440c
Techbot <techbot121@gmail.com>
2016-04-10 01:08:03 +0200

    let's wait 10 secs actually, because github rate limit

--- src/DiscordBot/Modules/Github/GithubModule.cs
+++ src/DiscordBot/Modules/Github/GithubModule.cs
@@ -291,7 +291,7 @@ namespace DiscordBot.Modules.Github
                                 continue;
                             }
 
-                            await Task.Delay(5000, cancelToken); //Wait 1 second between individual requests
+                            await Task.Delay(10000, cancelToken); //Wait 10 seconds between individual requests
                         }
                     }
                     await Task.Delay(60000, cancelToken); //Wait 1 minute between full updates
